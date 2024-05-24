ae022726e43b8d05d46b0f7292cab31feeb0b1a4
Joan <jseva@devcenter.es>
2024-05-24 19:23:21 +0200

    FINAL

--- WPF/gestiMed2024/gestiMed2024/Properties/AssemblyInfo.cs
+++ WPF/gestiMed2024/gestiMed2024/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("GestiMed")]
+[assembly: AssemblyTitle("gestiMed2024")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gesti Med")]
+[assembly: AssemblyProduct("gestiMed2024")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
